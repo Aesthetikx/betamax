e7fa8dabf7c33555237da79a495439856b37976b
John DeSilva <john@aesthetikx.info>
2026-06-15 18:01:16 -0400

    Remove broken spec

--- spec/betamax_spec.rb
+++ spec/betamax_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Betamax do
   it "has a version number" do
     expect(Betamax::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
